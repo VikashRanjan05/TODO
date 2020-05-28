889ed1eb0b0499ccd0e1dd2d302f082d6d55483c
Vikash Ranjan <vikashranjan327@gmail.com>
2020-05-28 20:47:17 +0530

    Updates

---
+++ precache-manifest.55143a6e2f0a4a29a74e15d1d9e068a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00acb3e84bd1f495f5fb078359328af2",
+    "url": "/TODO/index.html"
+  },
+  {
+    "revision": "fc154ab0024e126afd0e",
+    "url": "/TODO/static/css/main.362866d4.chunk.css"
+  },
+  {
+    "revision": "484536a0177f8c796abc",
+    "url": "/TODO/static/js/2.9c9b853a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TODO/static/js/2.9c9b853a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc154ab0024e126afd0e",
+    "url": "/TODO/static/js/main.f8ac1de0.chunk.js"
+  },
+  {
+    "revision": "e05b611148c93e7223ab",
+    "url": "/TODO/static/js/runtime-main.143b5746.js"
+  }
+]);
